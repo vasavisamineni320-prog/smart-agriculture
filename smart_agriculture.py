6afd256f14fd28c3f64f9275ed2a53b4d6b7b1c6
Vasavi <vasavisamineni320@gmail.com>
2026-03-27 10:29:05 +0530

    Create smart_agriculture.py

---
+++ smart_agriculture.py
@@ -0,0 +1,11 @@
+# Smart Agriculture - Soil Moisture System
+
+moisture = int(input("Enter soil moisture level:"))
+
+if moisture < 30
+  print("Soil is dry - Irrigation needed")
+elif moisture <= 70:
+  print("Soil moisture is optional")
+else:
+  print("Soil is too wet")
+  
